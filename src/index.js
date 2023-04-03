4372b15f446bfef089be78936f4b6f015a1232fb
Mikael Josias <mikaeljosias2002@gmail.com>
2023-04-03 09:49:16 -0300

    feat: specialties routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+import handleApplicationErrors from "./middlewares/errorMiddleware.js";
+import router from "./routes/index.js";
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import router from "./routes/index.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use(router);
+app.use(handleApplicationErrors);
 
 app.listen(process.env.PORT, () => {
     console.log(`SERVER INITIALIZED ON PORT ${process.env.PORT}`);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
+import specialtiesRouter from "./specialtiesRouter.js";
 import { Router } from "express";
 
 const router = Router();
+
+router.use("/specialties", specialtiesRouter);
 
 export default router;
