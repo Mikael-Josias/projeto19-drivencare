3bc4f12b71b4a83e24af62fbfe4d0d23c61b6107
Mikael Josias <mikaeljosias2002@gmail.com>
2023-04-03 11:06:11 -0300

    feat: doctors schema

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -4,6 +4,22 @@ const specialtySchema = Joi.object({
     name: Joi.string().max(100).required()
 });
 
+const doctorsSignUpSchema = Joi.object({
+    name: Joi.string().min(3).max(100).required(),
+    email: Joi.string().max(100).email().required(),
+    password: Joi.string().min(6).required(),
+    specialty_id: Joi.number().integer().positive().required(),
+    address: Joi.object({
+        cep: Joi.string().length(8).required(),
+        street: Joi.string().max(100).required(),
+        number: Joi.number().integer().positive().required(),
+        neighborhood: Joi.string().max(100).required(),
+        city: Joi.string().max(100).required(),
+        state: Joi.string().max(100).required()
+    }).required()
+});
+
 export default {
-    specialtySchema
+    specialtySchema,
+    doctorsSignUpSchema
 }
